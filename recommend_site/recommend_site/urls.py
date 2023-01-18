b8c505a93e06f214e908e3da2173417b8b0e0f8f
kuomat <kuomatthew20040529@gmail.com>
2023-01-18 16:25:44 -0500

    completed forgot password function

--- recommend_site/recommend_site/urls.py
+++ recommend_site/recommend_site/urls.py
@@ -22,19 +22,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name='register'),
 
-    path('password-reset/',
+    path('password_reset/',
          auth_views.PasswordResetView.as_view(template_name='register/password_reset.html'),
          name='password_reset'),
 
-    path('password-reset/done/',
+    path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='register/password_reset_done.html'),
          name='password_reset_done'),
 
-    path('password-reset-confirm/<uidb64>/<token>',
+    path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='register/password_reset_confirm.html'),
          name='password_reset_confirm'),
 
-    path('password-reset-complete/',
+    path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name='register/password_reset_complete.html'),
          name='password_reset_complete'),
 
