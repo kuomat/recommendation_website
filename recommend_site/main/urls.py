63bcdfdb348be8885fbb80943dd5863501d05595
kuomat <kuomatthew20040529@gmail.com>
2023-01-19 16:08:20 -0500

    Finished questions page

--- recommend_site/main/urls.py
+++ recommend_site/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.home, name='home'),
     path("home/", views.home, name='home'),
+    path("questions/<int:ratings>", views.questions, name='questions'),
 ]
--- recommend_site/main/views.py
+++ recommend_site/main/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-
-def index(response):
-    return render(response, "main/base.html", {})
+import random
+import pandas as pd
 
 # redirects to the login page if not logged in
 @login_required(login_url='/login/')
 def home(response):
+    if response.method == 'POST':
+        movie_nums = response.POST.get('movie_nums')
+        try:
+            int(movie_nums)
+            new_path = "/questions/" + movie_nums
+            return redirect(new_path)
+        except ValueError:
+            pass
     return render(response, "main/home.html", {})
+
+
+@login_required(login_url='/login/')
+def questions(response, ratings):
+    # randomly get the movies and put them in a list
+    movies_df = pd.read_csv("main/movies.csv")
+    movies = []
+    for _ in range(ratings):
+        index = random.randint(0, movies_df.shape[0] - 1)
+        movies.append(movies_df.loc[index, 'title'])
+
+    return render(response, "main/questions.html", {"movies": movies})
